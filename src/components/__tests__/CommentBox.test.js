933d887e030e15575a939052df3ab84bb4b938a6
Anurag Bhandari <anurag.bhd@gmail.com>
2020-06-07 14:02:20 +0530

    Add tests for CommentBox

---
+++ src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import CommentBox from '../CommentBox';
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = mount(<CommentBox />);
+});
+
+afterEach(() => {
+  wrapped.unmount();
+});
+
+describe('CommentBox', () => {
+  it('shows a textarea and a button', () => {
+    expect(wrapped.find('textarea').length).toEqual(1);
+    expect(wrapped.find('button').length).toEqual(1);
+  });
+
+  describe('textarea', () => {
+    const comment = 'hello, how are you';
+
+    beforeEach(() => {
+      const mockChangeEventObj = { target: { value: comment } };
+      wrapped.find('textarea').simulate('change', mockChangeEventObj);
+      wrapped.update(); // re-render the component as setState, being async, might not have finished yet
+    });
+
+    it('allows user to enter a comment', () => {
+      expect(wrapped.find('textarea').prop('value')).toEqual(comment);
+    });
+
+    it('is emptied on form submit', () => {
+      wrapped.find('form').simulate('submit');
+      wrapped.update();
+
+      expect(wrapped.find('textarea').prop('value')).toEqual('');
+    });
+  });
+});
