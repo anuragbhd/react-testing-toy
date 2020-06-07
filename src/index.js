926d3c96d3e878d4e335c781037e14fa42fb9321
Anurag Bhandari <anurag.bhd@gmail.com>
2020-06-07 19:13:11 +0530

    Set up a basic Redux store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import App from './components/App';
+import combinedReducer from './reducers';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+  <Provider
+    store={createStore(
+      combinedReducer,
+      {},
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
